3028808c3e4fdf3815aa557a764a3a7a629286d5
Jonathan "Won" Waters <kendoman96@gmail.com>
2020-01-25 08:19:24 -0500

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-speedtest > /out.txt
+TEST=$(speedtest)
+
+DOWN=$(echo $TEST | awk -F 'Download:' '{print $2}' | awk -F 'Mbit/s' '{print $1}' | awk -F '.' '{print $1}')
+UP=$(echo $TEST | awk -F 'Upload:' '{print $2}' | awk -F 'Mbit/s' '{print $1}' | awk -F '.' '{print $1}')
+DATE=$(date +%F)
+TIME=$(date +%T)
+
+echo "$DOWN, $UP, $DATE, $TIME" >> speedtest.log
