30118094327efe70b0b3518dd8e2042ffd9f456f
Jonathan "Won" Waters <kendoman96@gmail.com>
2021-01-22 06:42:17 -0500

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -2,9 +2,10 @@
 
 TEST=$(speedtest)
 
-DOWN=$(echo $TEST | awk -F 'Download:' '{print $2}' | awk -F 'Mbit/s' '{print $1}' | awk -F '.' '{print $1}')
-UP=$(echo $TEST | awk -F 'Upload:' '{print $2}' | awk -F 'Mbit/s' '{print $1}' | awk -F '.' '{print $1}')
+DOWN=$(echo $TEST | awk -F 'Download:' '{print $2}' | awk -F 'Mbit/s' '{print $1}' | awk -F '.' '{print $1}' | tr -s " " | xargs)
+UP=$(echo $TEST | awk -F 'Upload:' '{print $2}' | awk -F 'Mbit/s' '{print $1}' | awk -F '.' '{print $1}' | tr -s " " | xargs)
 DATE=$(date +%F)
 TIME=$(date +%T)
 
-echo "$DOWN, $UP, $DATE, $TIME" >> speedtest.log
+echo "$DOWN, $UP, $DATE, $TIME" > /srv/http/log/speedtest.last
+echo "$DOWN, $UP, $DATE, $TIME" >> /srv/http/log/speedtest.log
